accfdbfa4e2a69925f855ee13ca4c67303968d6b
sueda kurnaz <144630057+suedakurnaz@users.noreply.github.com>
2025-05-28 10:21:36 +0300

    Update quiz_logic.py

--- quiz_logic.py
+++ quiz_logic.py
@@ -1,15 +1,10 @@
-import json
-
 class QuizManager:
-    def __init__(self):
-        with open("questions.json", "r") as f:
-            self.questions = json.load(f)
+    def __init__(self, questions):
+        self.user_answers = []
+        self.questions = questions
 
     def get_question(self, index):
         return self.questions[index]
 
     def check_answer(self, index, selected):
         return self.questions[index]["answer"] == selected
-
-    def total_questions(self):
-        return len(self.questions)
